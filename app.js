5a5ec946824e1fcc4ad0c182f81e2e12b1992018
NehadAwad <nehadakash622@gmail.com>
2022-05-18 00:08:20 +0600

    weather api url added

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+const  request  = require("request")
+
+
+const url = 'http://api.weatherstack.com//current?access_key=e5bb45bb4d393912e95a17adcee9acc2&query=37.8267,-122.4233'
+
+request({ url: url}, (error, response) =>{
+    console.log(response)
+})
